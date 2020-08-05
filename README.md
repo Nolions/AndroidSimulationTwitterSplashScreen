6847c1c2d9ce685313bf14dfca15cc63ddb0cf29
KevinChen <lv2410@gmail.com>
2020-08-05 22:36:05 +0800

    [init]

---
+++ README.md
@@ -0,0 +1,5 @@
+# SimulationTwitterSplashScreen
+
+## Reference
+
+1. [Android MotionLayout: Creating the Twitter splash screen in the simplest way possible](https://proandroiddev.com/android-motionlayout-twitter-splash-screen-b5755ed56ee8)
---
+++ app/src/main/java/tw/nolions/motionlayout/MainActivity.kt
@@ -0,0 +1,35 @@
+package tw.nolions.motionlayout
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+
+        if (hasFocus) {
+            hideSystemUIAndNavigation(this)
+        }
+    }
+
+    private fun hideSystemUIAndNavigation(activity: Activity) {
+        val decorView: View = activity.window.decorView
+        decorView.systemUiVisibility =
+                (View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN // Hide the nav bar and status bar
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    }
+}
